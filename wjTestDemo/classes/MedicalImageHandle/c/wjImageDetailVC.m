36165e74ac43c96afd31fac9af69be9a24912961
wangjun <jerry@jerrydeMac.local>
2017-07-14 10:01:30 +0800

    完成影像资料的按钮界面的搭建

--- wjTestDemo/classes/MedicalImageHandle/c/wjImageDetailVC.m
+++ wjTestDemo/classes/MedicalImageHandle/c/wjImageDetailVC.m
@@ -8,19 +8,39 @@
 
 #import "wjImageDetailVC.h"
 #import "wjImageModel.h"
+#import "wjButtonModel.h"
 #import "wjButton.h"
 
+
 @interface wjImageDetailVC ()
+
+@property (nonatomic, strong) NSMutableArray *buttonsArray;
+
 
 @end
 
 @implementation wjImageDetailVC
 
+
+#pragma mark - 懒加载
+- (NSMutableArray *)buttonsArray {
+    if (!_buttonsArray) {
+        NSArray *buttons = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"wjButton" ofType:@"plist"]];
+        NSMutableArray *btnArr = [NSMutableArray array];
+        for (NSDictionary *dict in buttons) {
+            wjButtonModel *model = [wjButtonModel buttionWithButtonDict:dict];
+            [btnArr addObject:model];
+        }
+        _buttonsArray = [btnArr copy];
+    }
+    return _buttonsArray;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"图像处理";
     NSLog(@"model is : %@", (wjImageModel *)self.model.name);
-
     [self wjBasicUISettings];
     
 }
@@ -29,25 +49,34 @@
 #pragma mark - 基本的界面设置
 - (void)wjBasicUISettings {
     // 设置底部的按钮
-    NSArray *buttonArray = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"wjButton" ofType:@"plist"]];
-    NSMutableArray *buttons = [NSMutableArray array];
-    for (NSDictionary *dict in buttonArray) {
-        [buttons addObject:dict];
-    }
     CGFloat y = SCREEN_HEIGHT - 49;
-    CGFloat width = (CGFloat)SCREEN_WIDTH / buttons.count;
+    CGFloat width = (CGFloat)SCREEN_WIDTH / self.buttonsArray.count;
     CGFloat height = 49;
-    for (int i = 0; i < buttons.count; i++) {
+    for (int i = 0; i < self.buttonsArray.count; i++) {
         CGFloat x = i * width;
         wjButton *btn = [[wjButton alloc] init];
-        btn.backgroundColor = [UIColor blueColor];
+        btn.tag = i + 100; // 设tag值
         btn.frame = CGRectMake(x, y, width, height);
-        btn.model = buttons[i];
+        btn.model = self.buttonsArray[i];
+        [btn addTarget:self action:@selector(btnClickAction:) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:btn];
     }
     
 }
 
+
+static bool isClicked = NO;
+- (void)btnClickAction:(UIButton *)btn {
+    NSLog(@"%ld", btn.tag);
+//    if (!isClicked) {
+//        btn.backgroundColor = [UIColor blueColor];
+//        
+//        isClicked = !isClicked;
+//    }
+    btn.backgroundColor = isClicked ? [UIColor whiteColor] : [UIColor blueColor];
+    isClicked = !isClicked;
+    
+}
 
 
 
--- wjTestDemo/classes/MedicalImageHandle/v/wjButton.m
+++ wjTestDemo/classes/MedicalImageHandle/v/wjButton.m
@@ -16,6 +16,7 @@
     if (self = [super initWithFrame:frame]) {
         // 文本居中
         self.titleLabel.textAlignment = NSTextAlignmentCenter;
+        self.imageView.contentMode = UIViewContentModeCenter;
         // 文字颜色
         [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         self.titleLabel.font = [UIFont systemFontOfSize:12.0f];
@@ -28,10 +29,12 @@
     // 获取自身的尺寸
     CGFloat width = self.frame.size.width;
     CGFloat height = self.frame.size.height;
-    
     // 调整子控件的位置
-    self.imageView.frame = CGRectMake(0, 0, width, width);
-    self.titleLabel.frame = CGRectMake(0, width, width, height - width);
+    CGFloat imageViewY = 34;
+    self.imageView.frame = CGRectMake(0, 0, width, imageViewY); // 30是图片的高度
+    CGFloat imageY = CGRectGetMaxY(self.imageView.frame);
+    self.titleLabel.frame = CGRectMake(0, imageY, width, height - imageY);
+    
 }
 
 - (void)setModel:(wjButtonModel *)model {
